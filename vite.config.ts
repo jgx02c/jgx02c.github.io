dc04f16b8164f3050e5377aae0614059cc144828
Joshua <97928637+jgx02c@users.noreply.github.com>
2025-03-10 15:10:45 -0700

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,16 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
-    base: '/jgx02c.github.io/',
+    base: './',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
+    }
 });
